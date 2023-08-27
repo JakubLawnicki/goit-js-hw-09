ad9963b0cb4d824e00655ff2603ab826cd5a53ee
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-27 21:31:12 +0200

    Color switcher added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let randomColor;
+let colorChange;
+
+startButton.addEventListener('click', () => {
+  colorChange = setInterval(() => {
+    randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    startButton.setAttribute('disabled', 'disabled');
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(colorChange);
+  startButton.removeAttribute('disabled', 'disabled');
+});
