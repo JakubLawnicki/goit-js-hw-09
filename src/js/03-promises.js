16622f292205b626288d0f12d6af58336212678b
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-29 00:45:10 +0200

    Promises generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+  promise
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
+
+const inputDelay = document.querySelector("input[name='delay']");
+const inputStep = document.querySelector("input[name='step']");
+const inputAmount = document.querySelector("input[name='amount']");
+const button = document.querySelector('button');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  setTimeout(() => {
+    createPromise(1, inputDelay.value);
+  }, inputDelay.value);
+  const interval = setInterval(() => {
+    for (let i = 2; i <= inputAmount.value; i++) {
+      createPromise(i, inputStep.value);
+    }
+    clearInterval(interval);
+  }, inputStep.value);
+});
