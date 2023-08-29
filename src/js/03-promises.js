e6bc010d43fd6cfd330f23b0aa1a507bdba60fcd
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-29 22:38:45 +0200

    Notiflix added

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,6 @@
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   const promise = new Promise((resolve, reject) => {
@@ -9,10 +12,12 @@ function createPromise(position, delay) {
   });
   promise
     .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     });
 }
 
